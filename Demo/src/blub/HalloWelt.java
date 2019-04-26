f5aae5295ef59e3bccc27a2da79dbb08b493ab83
kuti <kuti@WNC01040.ad.fh-albsig.de>
2019-04-26 13:35:18 +0200

    und auf ein neues

--- Demo/src/blub/HalloWelt.java
+++ Demo/src/blub/HalloWelt.java
@@ -5,7 +5,7 @@ public class HalloWelt {
 	public static void main(String[] args) {
 		System.out.println("Hallo Welt");
 
-		
+		System.out.println("ich bin doch nicht alt :)");
 		System.out.println("und noch ne änderung hehehe");
 	}
 
